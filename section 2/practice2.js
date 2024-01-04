423492acd0fef7f7dbb8bae1c979177a7de83d7b
seunghyun333 <shb2in33@naver.com>
2024-01-05 00:25:56 +0900

    연습문제2 Math.ceil

---
+++ section 2/practice2.js
@@ -0,0 +1,10 @@
+// 분기별 일정
+
+//해당 월을 입력하면 몇 분기인지 number 형태로 알려주는 알고리즘을 만들려 합니다.
+//특정 월이 month로 주어질 때, month는 몇 분기에 해당하는지 return 하는 함수 solution을 완성하세요.
+
+
+function solution(month) {
+  return Math.ceil(month / 3); //올림
+  }
+  
