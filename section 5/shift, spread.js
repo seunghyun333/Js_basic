7635cd6aa128d3b3eeef516ef58e46268ced76d7
seunghyun333 <shb2in33@naver.com>
2024-01-15 23:33:09 +0900

    save

--- section 5/shift, spread.js
+++ section 5/shift, spread.js
@@ -1,24 +1,28 @@
-/* 프로젝트 명세서
+/* 좋은 질문? 지적? 아무튼 감사합니다.
 
-프로젝트에서 사용된 기술 스택의 이름이 배열 stacks 으로 주어질 때
-내림차순으로 정렬하여 return하는 함수 solution 완성하기
+지난 번 발표한 사람들의 닉네임이 순서대론 담긴 배열 prev 와
+새로 참여한 사람의 닉네임이 문자열 join으로 주어질 때
+이번 스터디의 발표 순서가 담긴 배열을 return하는 함수 solution을 완성하기
 
 조건:
-stacks의 길이는 1 이상 10 이하
-stacks의 각 단어의 길이는 1 이상 10 이하의 문자열
-stacks의 단어들은 영문 소문자로만 이루어져 있음
+join 은 길이 1 이상 10 이하인 문자열
+배열 prev는 길이 2 이상 10 이하 
+
 
 */
 
 /* 첫 번째 답 (조건을 생각하지 않음)
-function soluction(stacks) {
-    return stakcs.sort();
+function soluction(prev, join) {
+    const last = prev.pop();
+    return prev.pop().shift(join).push(last);
 }
 */
 
-// sort() 오름차순
-// reverse() 반대로
+// shift : 배열에서 맨 처음 요소를 제거해주고 그 제거한 요소를 결과로 반환해주는 매서드 
+// 스프레드 문법: 배열앞에 ...을 붙여 배열의 요소 꺼내오기
 
-function solution(stacks) {
-    return stacks.sort().reverse();
+function solution(prev, join) {
+    // [join, 나머지, 지난번_처음 발표자]
+    const 지난발표자 = prev.shift();
+    return [join, ...prev, 지난발표자];
 }
--- section 7/while.js
+++ section 7/while.js
@@ -5,5 +5,8 @@
 let 나이 = 16;
 while (나이 < 16) {
     console.log(나이, "입장불가");
+    if(나이 === 19) continue;
+   //if(나이 === 19) break; return을 써도 됨
     나이 = 나이+1;
 }
+console.log("끝");
