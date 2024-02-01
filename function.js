eba507f434f57a1af42aedf65caf6c32e588f217
승현 <128073991+seunghyun333@users.noreply.github.com>
2024-02-01 11:36:33 +0900

    Update function.js

--- function.js
+++ function.js
@@ -1,4 +1,6 @@
-  function initMain() {
+<!-- 초기화 할 변수를 함수에 넣고 함수를 호출하면 간단하게 초기화 가능 // 1) 가독성 측면 2) 전역객체를 보호 -->
+
+function initMain() {
 	  
 		//[1]Init All Single-Buttons Here
 		$("#FwEngSearchButton").click(function(){ clickSearchButton(); return false;});      //조회 버튼 클릭
